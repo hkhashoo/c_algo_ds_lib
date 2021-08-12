a8edbb13be982bbdb0b6a708aff254f8a4f403e5
Vip-Bhardw <vipulbhardwaj1011@gmail.com>
2021-08-12 20:18:11 +0530

    insertOne and helper insertion function added

--- BST/bst.c
+++ BST/bst.c
@@ -24,7 +24,25 @@ void preOrder(BST *root, void (*myPrint)(void*)) {
     }
 }
 
-BST *newBST(int (*myCompare)(void*, void*), int elements, ...) {
+void insertion(int(*myCompare)(void*, void*), BST *root, void *val) {
+    BST *temp = root;
+    BST *prev = temp;
+
+    while(temp != NULL) {
+        prev = temp;
+        if(myCompare(temp->data, val)) temp = temp->right;
+        else temp = temp->left;
+    }
+    BST *newNode = malloc(sizeof(BST));
+    setData(newNode, val);
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    if(myCompare(prev->data, val)) prev->right = newNode;
+    else prev->left = newNode;
+}
+
+BST *newBST(int(*myCompare)(void*, void*), void(*setData)(BST*, void*), int elements, ...) {
     BST *root = NULL;
     void *val;
     va_list ap;
@@ -36,26 +54,17 @@ BST *newBST(int (*myCompare)(void*, void*), int elements, ...) {
 
         if(root == NULL) {
             root = malloc(sizeof(BST));
-            root->data = val;
+            setData(root, val);
             root->right = NULL;
             root->left = NULL;
-        } else {
-            BST *temp = root;
-            BST *prev = temp;
-
-            while(temp != NULL) {
-                prev = temp;
-                if(myCompare(root->left, val)) temp = temp->right;
-                else temp = temp->left;
-            }
-            BST *newNode = malloc(sizeof(BST));
-            newNode->data = val;
-            newNode->left = NULL;
-            newNode->right = NULL;
-
-            if(myCompare(root->left, val)) prev->right = newNode;
-            else prev->left = newNode;
-        }
+        } else insertion(myCompare, root, val);
     }
     return root;
+}
+
+void insertOne(int(*myCompare)(void*, void*), void(*setData)(BST*, void*), BST *root, void *data) {
+    BST *newNode = malloc(sizeof(BST));
+    setData(newNode, data);
+
+    insertion(myCompare, root, data);
 }
--- BST/bst.h
+++ BST/bst.h
@@ -7,7 +7,11 @@ typedef struct BST {
 	struct BST *right;
 } BST;
 
-BST* newBST(int (*)(void*, void*), int, ...);
+BST* newBST(int (*)(void*, void*), void (*)(BST*, void*), int, ...);
+void insertion (int (*)(void*, void*), BST*, void*);
+void setData(BST*, void*);
 void inOrder(BST*, void (*)(void*));
 void preOrder(BST*, void (*)(void*));
 void postOrder(BST*, void (*)(void*));
+void insertOne(int (*)(void*, void*), void (*)(BST*, void*), BST*, void*);
+void insertMany(int (*)(void*, void*), void (*)(BST*, void*), int, ...);
