eb7fc76382bc3d585ae58696489b2830686dfc33
Vip-Bhardw <vipulbhardwaj1011@gmail.com>
2021-08-21 22:31:25 +0530

    added all deletion functions

--- Lists/Circular Linked List/cll.h
+++ Lists/Circular Linked List/cll.h
@@ -8,3 +8,9 @@ typedef struct CLL {
 
 CLL* newCLL(int, ...);
 void insertion(CLL*, void*);
+void insertFront(CLL**, void*);
+int deleteIndex(CLL**, int, ...);
+int length(CLL*);
+int deleteKeys(int (*)(void*, void*), CLL**, int, ...);
+int keyCount(int (*)(void*, void*), CLL*, void*);
+int delete(int (*)(void*, void*), CLL**, void*);
